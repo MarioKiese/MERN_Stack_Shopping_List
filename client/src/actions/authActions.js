7ad7f86a58175b24768bc2ee957d69666023f039
Mario Kiese <mali.mk73@googlemail.com>
2020-06-07 02:52:48 +0200

    React & Redux Auth State

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { returnErrors } from "./errorActions";
+
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+} from "./types";
+
+// Check token & load user
+export const loadUser = () => (dispatch, getState) => {
+  // User loading
+  dispatch({ type: USER_LOADING });
+
+  axios
+    .get("/api/auth/user", tokenConfig(getState))
+    .then((res) =>
+      dispatch({
+        tpye: USER_LOADED,
+        payload: res.data,
+      })
+    )
+    .catch((error) => {
+      dispatch(returnErrors(error.response.data, error.response.status));
+      dispatch({
+        type: AUTH_ERROR,
+      });
+    });
+};
+
+// Setup config/headers and token
+export const tokenConfig = (getState) => {
+  // Get token from localstorage
+  const token = getState().auth.token;
+
+  // Headers
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+
+  // If token, add to headers
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+
+  return config;
+};
